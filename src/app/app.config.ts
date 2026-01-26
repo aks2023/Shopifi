808845bccbe452a15bd241c884a10b72fd4ef497
Ayush Kumar Sinha <s.ayush@COLUMBIAASIA.COM>
2026-01-26 14:45:45 +0530

    modified app config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,9 +17,41 @@ import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-confi
 import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "shopifi-ba065", appId: "1:830520642675:web:8c65a32f400d2aa94f6e06", storageBucket: "shopifi-ba065.firebasestorage.app", apiKey: "AIzaSyAPVvTh4YnE_xY4r9KLa_XACmBSzrQo9Kg", authDomain: "shopifi-ba065.firebaseapp.com", messagingSenderId: "830520642675", measurementId: "G-38FTG9QFZK", projectNumber: "830520642675", version: "2" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
-  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-}), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideDataConnect(() => getDataConnect({connector: "example",location: "us-east4",service: "shopifi"})), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "shopifi-ba065",
+      appId: "1:830520642675:web:8c65a32f400d2aa94f6e06",
+      storageBucket: "shopifi-ba065.firebasestorage.app",
+      apiKey: "AIzaSyAPVvTh4YnE_xY4r9KLa_XACmBSzrQo9Kg",
+      authDomain: "shopifi-ba065.firebaseapp.com",
+      messagingSenderId: "830520642675",
+      measurementId: "G-38FTG9QFZK"
+      // Removed: projectNumber and version (not valid FirebaseOptions properties)
+    })),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideAppCheck(() => {
+      // Get your reCAPTCHA Enterprise site key from:
+      // https://console.cloud.google.com/security/recaptcha?project=shopifi-ba065
+      const provider = new ReCaptchaEnterpriseProvider('YOUR_RECAPTCHA_SITE_KEY_HERE');
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    }),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideDataConnect(() => getDataConnect({
+      connector: "example",
+      location: "us-east4",
+      service: "shopifi"
+    })),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI())
+  ]
 };
