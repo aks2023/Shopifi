f180ce7fe72bb620b9e9c9e4e556be05194ca28f
Ayush Kumar Sinha <s.ayush@COLUMBIAASIA.COM>
2026-01-26 14:58:57 +0530

    Project Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/app';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shopifi';
+   constructor(private readonly firebaseApp: FirebaseApp) {}
+
+  ngOnInit() {
+    console.log('🔥 Firebase App initialized:', this.firebaseApp.name);
+  }
 }
